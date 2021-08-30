e6fdcd722d4dc02e10b8ff30daeb787f69ed1b18
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2021-08-30 12:55:19 -0300

    finished app ()

--- src/controllers/ComplimentController.ts
+++ src/controllers/ComplimentController.ts
@@ -3,12 +3,14 @@ import { ComplimentsService } from "../services/ComplimentsService";
 
 export class ComplimentController {
   async handle(req: Request, res: Response) {
-    const { user_sender, user_receiver, tag_id, message } = req.body;
+    const { user_receiver, tag_id, message } = req.body;
+
+    const { user_id } = req;
 
     const complimentController = new ComplimentsService();
 
     const compliment = await complimentController.execute({
-      user_sender,
+      user_sender: user_id,
       user_receiver,
       tag_id,
       message,
